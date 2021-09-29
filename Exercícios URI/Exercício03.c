12a040e35e8e0c2b6c3a890e26e39111d3fd27ea
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2021-09-29 09:43:32 -0300

    update

--- Exercícios URI/Exercício03.c
+++ Exercícios URI/Exercício03.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 int main(void) {
-    int valor, numeroNotas[7], resto, n;
+    int valor, numeroNotas[7], resto;
     int notas[7] = {100, 50, 20, 10, 5, 2, 1};
     do
     {
@@ -10,12 +10,10 @@ int main(void) {
     } while (valor > 1000000 || valor < 0);
     for (int i = 0; i < 7; i++)
     {
-        //printf("%i\n", valor);
         resto =  floor(valor%notas[i]);
         numeroNotas[i] = floor((valor)/notas[i]);
         valor = resto;        
     }
-
     for (int i = 0; i < 7; i++)
     {
         printf("%d nota(s) de  R$ %d,00\n", numeroNotas[i], notas[i]);
