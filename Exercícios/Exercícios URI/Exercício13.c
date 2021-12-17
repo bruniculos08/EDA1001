1538b52f348c05a1ef1b3aff96bc37dc86c4a325
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2021-12-16 22:46:18 -0300

    update

--- Exercícios/Exercícios URI/Exercício13.c
+++ Exercícios/Exercícios URI/Exercício13.c
@@ -14,7 +14,7 @@ struct Pilha {
 };
 
 void insere(pilha *a, char h){
-    printf("inserindo...\n");
+    //printf("inserindo...\n");
     node *newNode;
     newNode = (node *)malloc(sizeof(node));
     newNode->nome = h;
@@ -38,14 +38,26 @@ void retira(pilha *a){
 }
 
 void insereN(pilha *a, int k, char *string){
-    int n;
-    if(a->topo == NULL) return;
-    n = a->topo->nome;
     for(int i = 0; i < k; i++) insere(a, string[i]);
 }
 
 void retiraN(pilha *a, int k){
     for(int i = 0; i < k && a->topo != NULL; i++) retira(a);
+}
+
+int verifica(pilha *a, char string[]){
+    if(a->topo == NULL) return 1;
+    node *b;
+    b = a->topo;
+    int r = 0;
+    for(int i = 0; i < 4; i++){
+        if(string[i] != b->nome){
+            r = 1;
+            break;
+        }
+        b = b->next;
+    }
+    return r;
 }
 
 void imprime(pilha *a){
@@ -70,34 +82,24 @@ int main(){
     {
         scanf("%i", &n);
     } while (n < 0 || n > 100);
-    char string[4];
+    char *string;
+    string = (char *)malloc(4*sizeof(char));
     pilha *a;
     node *b;
     a = (pilha*)malloc(sizeof(pilha));
     a->topo = NULL;
-    insere(a, 'F');
-    insere(a, 'A');
-    insere(a, 'C');
-    insere(a, 'E');
-    imprime(a);
+    insereN(a, 4, "FACE");
     for(int i = 0; i < n; i++){
         fflush(stdin);
-        for(int j = 0; j < 4; j++) scanf("%c", string[j]);
-        //scanf("%s", string);
-        if(a->topo != NULL){
-            b = a->topo;
-            for(int j = 0; j < 4; j++){
-                if(b->nome != string[j]){
-                    insereN(a, 4, string);
-                    break;
-                }
-                if(b != NULL) b = b->next;
-                if(j == 3){
-                    k++;
-                    retiraN(a, 4);
-                }
-            }
+        scanf("%c %c %c %c", &string[0], &string[1], &string[2], &string[3]);
+        if(verifica(a, string) == 0){
+            k++;
+            retiraN(a, 4);
+            if(a->topo == NULL) insereN(a, 4, "FACE");
         }
+        else insereN(a, 4, string);
+        //printf("Lista: ");
+        //imprime(a);
     }
     printf("%i\n", k);
 }
